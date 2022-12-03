c27422278c16369b0afc7689b92e5393d0cf6388
hyunu529 <hyunu529@naver.com>
2022-12-03 09:08:40 +0900

    changes the selection.

---
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,103 @@
+package com.example.compose.rally
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+//    Add Tests
+
+    @Test
+    fun myTest() {
+        composeTestRule.setContent {
+            Text("you can set any Compose content!")
+        }
+    }
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarChangeStateTest() {
+        val allScreens = RallyScreen.values().toList()
+        var currentScreen =  mutableStateOf(RallyScreen.Accounts)
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = currentScreen.value
+            )
+        }
+
+        currentScreen.value = RallyScreen.Bills
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Bills.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Bills.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+
+    }
+}
